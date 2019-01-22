ec9ff517b13ac0db396301b579e2f8bc5c766223
Gustavo Lopes <gustavo.lopes@ericsson.com>
2019-01-22 21:10:43 -0200

    Docker

--- controllers/helloController.py
+++ controllers/helloController.py
@@ -3,7 +3,7 @@ from flask_restful import Resource
 class HelloController(Resource):
 
     def get(self):
-        return {"response" : "hello get"} 
+        return {"response" : "hello funcionou"}
     
     def post(self):
         return {"response" : "hello post"}
