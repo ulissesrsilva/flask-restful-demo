207d686ae4a203050c38fe2a71e404d0117bd723
Ulisses Silva <ulissesrdasilva@gmail.com>
2019-01-22 22:13:42 -0200

    Integração com Docker

--- .circleci/heroku_deploy.sh
+++ .circleci/heroku_deploy.sh
@@ -3,5 +3,5 @@ set -u
 source .circleci/setup_env.sh
 echo "Pushing branch ${CIRCLE_BRANCH} to app ${HEROKU_APP}"
 git remote add heroku https://heroku:$HEROKU_API_KEY@git.heroku.com/${HEROKU_APP}.git
-git push -f heroku ${CIRCLE_BRANCH}:master
 heroku stack:set container
+git push -f heroku ${CIRCLE_BRANCH}:master
